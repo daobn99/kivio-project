d1f6e94af30aca7be1bee826fc8faac178ce1be8
Dao Nguyen <lmb29x178z@gmail.com>
2026-05-23 23:26:36 +0900

    initial commit

---
+++ kivio-backend/src/main/java/io/kivio/KivioBackendApplication.java
@@ -0,0 +1,13 @@
+package io.kivio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KivioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KivioBackendApplication.class, args);
+	}
+
+}
---
+++ kivio-backend/src/test/java/io/kivio/KivioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kivio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KivioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
